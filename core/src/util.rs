9f7b6951e67b6d466e9e992ddbd2d7d5a43c9dba
yuekcc <yuekcc@qq.com>
2022-01-30 22:28:48 +0800

    map 改为 while iter

--- core/src/util.rs
+++ core/src/util.rs
@@ -14,28 +14,46 @@ pub async fn load_md_contents(url: &str) -> anyhow::Result<String> {
     Ok(text)
 }
 
+fn is_abs_uri(uri: &str) -> bool {
+    let _uri = uri.trim().to_lowercase();
+    _uri.starts_with("http://") || _uri.starts_with("http://") || _uri.starts_with("//")
+}
+
 pub fn render_markdown(doc: &str) -> String {
-    let parser = Parser::new_ext(doc, Options::all());
+    let mut parser = Parser::new_ext(doc, Options::all());
 
-    let parser = parser.map(|event| match event {
-        Event::Start(Tag::Link(link_type, dest, title)) => {
-            let _dest = dest.to_lowercase();
-
-            if _dest.starts_with("https://")
-                || _dest.starts_with("http://")
-                || _dest.starts_with("//")
-            {
-                Event::Start(Tag::Link(link_type, dest, title)).into()
-            } else {
-                let rewired = format!("/#/{}", dest).replace("//", "/");
-                Event::Start(Tag::Link(link_type, CowStr::from(rewired), title)).into()
+    let mut parsed = Vec::new();
+    let mut get_heading = false;
+    while let Some(event) = parser.next() {
+        let _event = match event {
+            Event::Start(Tag::Link(link_type, dest, title)) => {
+                if is_abs_uri(&dest) {
+                    Event::Start(Tag::Link(link_type, dest, title)).into()
+                } else {
+                    let rewired = format!("/#/{}", dest).replace("//", "/");
+                    Event::Start(Tag::Link(link_type, CowStr::from(rewired), title)).into()
+                }
             }
-        }
-        _ => event,
-    });
+            Event::Start(Tag::Heading(level, id, class_list)) => {
+                get_heading = true;
+
+                Event::Start(Tag::Heading(level, id, class_list))
+            }
+            Event::Text(text) => {
+                if get_heading {
+                    log::debug!("get heading: {}", text);
+                }
+
+                Event::Text(text)
+            }
+            _ => event,
+        };
+
+        parsed.push(_event);
+    }
 
     let mut output = String::new();
-    html::push_html(&mut output, parser);
+    html::push_html(&mut output, parsed.into_iter());
 
     output
 }
