b6c61bfc1745727627abe0002abcaddc73207529
yuekcc <yuekcc@qq.com>
2022-02-12 00:34:39 +0800

    切换文章后，回到顶部

--- core/src/components.rs
+++ core/src/components.rs
@@ -1,6 +1,10 @@
 use sycamore::{futures::ScopeSpawnFuture, prelude::*};
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::Element;
 
-use crate::util::{highlight_all, load_md_contents, render_markdown};
+use crate::config::Config;
+use crate::router::Router;
+use crate::util::{highlight_all, load_md_contents, render_markdown, render_one_markdown};
 
 #[component]
 pub fn Post<'a, G: Html>(ctx: ScopeRef<'a>, md_src: &'a ReadSignal<String>) -> View<G> {
@@ -25,5 +29,100 @@ pub fn Post<'a, G: Html>(ctx: ScopeRef<'a>, md_src: &'a ReadSignal<String>) -> V
 
     view! {ctx,
         div(class="markdown-body", dangerously_set_inner_html=&(*doc.get()))
+    }
+}
+
+fn on_popstate(f: Box<dyn FnMut()>) {
+    let closure = Closure::wrap(f);
+
+    web_sys::window()
+        .unwrap()
+        .add_event_listener_with_callback("popstate", closure.as_ref().unchecked_ref())
+        .unwrap();
+
+    log::debug!("on_popstate");
+    closure.forget();
+}
+
+#[component]
+pub fn App<G: Html>(ctx: ScopeRef, props: &Config) -> View<G> {
+    let root = props.get_root_path();
+    let title = props.get_title();
+    let footer_message = render_one_markdown(&props.get_footer_message());
+
+    let main_md = create_rc_signal(String::new());
+    let sidebar_md = create_rc_signal(format!("{}{}", root, "SIDEBAR.md"));
+    let header_ref = ctx.create_node_ref();
+
+    {
+        let router = Router::new();
+
+        let main_md = main_md.clone();
+        let root = props.get_root_path();
+        let update_route = Box::new(move || {
+            let (path, _) = router.route().unwrap();
+            let home_page = format!("{}{}", root, "README.md").replace("//", "/");
+            let path = format!("{}{}", root, path).replace("//", "/");
+
+            log::info!("home_page = {}", home_page);
+            log::info!("path = {}", path);
+
+            let path = if path == root.as_str() {
+                home_page
+            } else {
+                path
+            };
+            main_md.set(path);
+        });
+
+        update_route();
+        on_popstate(update_route);
+    }
+
+    let _main_md = {
+        let main_md = main_md.clone();
+        ctx.create_memo(move || (*main_md.get()).clone())
+    };
+    let _sidebar_md = {
+        let sidebar_md = sidebar_md.clone();
+        ctx.create_memo(move || (*sidebar_md.get()).clone())
+    };
+
+    // 切换文章后，回到顶部
+    ctx.create_effect(|| {
+        _main_md.track();
+
+        let _header_ref = header_ref.clone();
+        ctx.spawn_future(async move {
+            log::info!("scroll to header");
+            let el = _header_ref.try_get::<DomNode>();
+            let el = el.unwrap();
+            el.unchecked_into::<Element>().scroll_into_view();
+        });
+    });
+
+    view! {ctx,
+        header {
+            div(ref=header_ref) {
+                a(href=root) {
+                    (title)
+                }
+            }
+        }
+        div(class="wrapper") {
+            div(class="post-wrapper") {
+                article(class="post") {
+                    Post(_main_md)
+                }
+                aside(class="aside") {
+                    div(class="content-wrapper") {
+                        Post(_sidebar_md)
+                    }
+                }
+            }
+        }
+        footer {
+            div(dangerously_set_inner_html=&footer_message)
+        }
     }
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,98 +1,15 @@
-use config::Config;
-use router::Router;
 use sycamore::prelude::*;
-use util::render_one_markdown;
-use wasm_bindgen::{prelude::*, JsCast};
+use wasm_bindgen::prelude::*;
 
 mod components;
 mod config;
 mod router;
 mod util;
 use components::*;
+use config::Config;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-fn on_popstate(f: Box<dyn FnMut()>) {
-    let closure = Closure::wrap(f);
-
-    web_sys::window()
-        .unwrap()
-        .add_event_listener_with_callback("popstate", closure.as_ref().unchecked_ref())
-        .unwrap();
-
-    log::debug!("on_popstate");
-    closure.forget();
-}
-
-#[component]
-fn App<G: Html>(ctx: ScopeRef, props: &Config) -> View<G> {
-    let root = props.get_root_path();
-    let title = props.get_title();
-    let footer_message = render_one_markdown(&props.get_footer_message());
-
-    let main_md = create_rc_signal(String::new());
-    let sidebar_md = create_rc_signal(format!("{}{}", root, "SIDEBAR.md"));
-
-    {
-        let router = Router::new();
-
-        let main_md = main_md.clone();
-        let root = props.get_root_path();
-        let update_route = Box::new(move || {
-            let (path, _) = router.route().unwrap();
-            let home_page = format!("{}{}", root, "README.md").replace("//", "/");
-            let path = format!("{}{}", root, path).replace("//", "/");
-
-            log::info!("home_page = {}", home_page);
-            log::info!("path = {}", path);
-
-            let path = if path == root.as_str() {
-                home_page
-            } else {
-                path
-            };
-            main_md.set(path);
-        });
-
-        update_route();
-        on_popstate(update_route);
-    }
-
-    let _main_md = {
-        let main_md = main_md.clone();
-        ctx.create_memo(move || (*main_md.get()).clone())
-    };
-    let _sidebar_md = {
-        let sidebar_md = sidebar_md.clone();
-        ctx.create_memo(move || (*sidebar_md.get()).clone())
-    };
-
-    view! {ctx,
-        header {
-            div {
-                a(href=root) {
-                    (title)
-                }
-            }
-        }
-        div(class="wrapper") {
-            div(class="post-wrapper") {
-                article(class="post") {
-                    Post(_main_md)
-                }
-                aside(class="aside") {
-                    div(class="content-wrapper") {
-                        Post(_sidebar_md)
-                    }
-                }
-            }
-        }
-        footer {
-            div(dangerously_set_inner_html=&footer_message)
-        }
-    }
-}
 
 #[wasm_bindgen]
 pub fn main(config: &Config) {
